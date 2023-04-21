53023614b31e3ff38e8deeae59f7a7e2cecd2c3f
Admin <urmasuper93@gmail.com>
2023-04-21 22:35:51 +0300

    first commit

---
+++ app/src/main/java/com/example/myfirstcrmsystem/firstfragment/FirstAdaptor.kt
@@ -0,0 +1,25 @@
+package com.example.myfirstcrmsystem.firstfragment
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class FirstAdaptor : RecyclerView.Adapter<FirstAdaptor.FirstViewHolder>()
+{
+    class FirstViewHolder(view:View) : RecyclerView.ViewHolder(view){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FirstViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: FirstViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
